d5e0c89bf6a067360f71adf4d11b7d5b9fc4c4d0
Zutatensuppe <oo.para@gmail.com>
2021-05-15 17:39:54 +0200

    combine wsclient and wswrapper

--- public/WsClient.js
+++ public/WsClient.js
@@ -1,10 +1,32 @@
 "use strict"
 
-import WsWrapper from './WsWrapper.js'
+import Time from '../common/Time.js'
 
-export default class WsClient extends WsWrapper {
+const CODE_CUSTOM_DISCONNECT = 4000
+
+/**
+ * Wrapper around ws that
+ * - buffers 'send' until a connection is available
+ * - automatically tries to reconnect on close
+ */
+export default class WsClient {
+  // actual ws handle
+  handle = null
+
+  // timeout for automatic reconnect
+  reconnectTimeout = null
+
+  // buffer for 'send'
+  sendBuffer = []
+
   constructor(addr, protocols) {
-    super(addr, protocols)
+    this.addr = addr
+    this.protocols = protocols
+
+    this.onopen = () => {}
+    this.onclose = () => {}
+    this.onmessage = () => {}
+
     this._on = {}
     this.onopen = (e) => {
       this._dispatch('socket', 'open', e)
@@ -20,6 +42,50 @@ export default class WsClient extends WsWrapper {
     }
     this.onclose = (e) => {
       this._dispatch('socket', 'close', e)
+    }
+  }
+
+  send (txt) {
+    if (this.handle) {
+      this.handle.send(txt)
+    } else {
+      this.sendBuffer.push(txt)
+    }
+  }
+
+  connect() {
+    let ws = new WebSocket(this.addr, this.protocols)
+    ws.onopen = (e) => {
+      if (this.reconnectTimeout) {
+        clearTimeout(this.reconnectTimeout)
+      }
+      this.handle = ws
+      // should have a queue worker
+      while (this.sendBuffer.length > 0) {
+        this.handle.send(this.sendBuffer.shift())
+      }
+      this.onopen(e)
+    }
+    ws.onmessage = (e) => {
+      this.onmessage(e)
+    }
+    ws.onerror = (e) => {
+      this.handle = null
+      this.reconnectTimeout = setTimeout(() => { this.connect() }, 1 * Time.SEC)
+      this.onclose(e)
+    }
+    ws.onclose = (e) => {
+      this.handle = null
+      if (e.code !== CODE_CUSTOM_DISCONNECT) {
+        this.reconnectTimeout = setTimeout(() => { this.connect() }, 1 * Time.SEC)
+      }
+      this.onclose(e)
+    }
+  }
+
+  disconnect() {
+    if (this.handle) {
+      this.handle.close(CODE_CUSTOM_DISCONNECT)
     }
   }
 
