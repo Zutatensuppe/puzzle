c08814de0732d6682682b74fb91b6f61d61b5687
Zutatensuppe <oo.para@gmail.com>
2021-12-17 00:22:03 +0100

    remove overlay transition

--- src/frontend/components/Overlay.vue
+++ src/frontend/components/Overlay.vue
@@ -1,24 +1,16 @@
 <template>
-  <transition name="overlay"
-    :duration="animate ? { enter: 300, leave: 250 } : 0"
-    @before-enter="classes=['enter-from']"
-    @enter="classes=['enter-active']"
-    @after-enter="classes=[]"
-    @before-leave="classes=['leave-to', 'leave-active']"
-  >
-    <div class="overlay">
-      <div
-        class="overlay-background"
-        @click="$emit('bgclick')"
-        :class="classes.map(c => 'overlay-background-' + c)">
-      </div>
-      <div
-        class="overlay-content"
-        :class="classes.map(c => 'overlay-content-' + c)">
-        <slot />
-      </div>
+  <div class="overlay">
+    <div
+      class="overlay-background"
+      @click="$emit('bgclick')"
+      :class="classes.map(c => 'overlay-background-' + c)">
     </div>
-  </transition>
+    <div
+      class="overlay-content"
+      :class="classes.map(c => 'overlay-content-' + c)">
+      <slot />
+    </div>
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
