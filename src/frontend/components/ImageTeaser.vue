644229d3afc3fa8fe611c96bb1cf7877d94589ac
Zutatensuppe <oo.para@gmail.com>
2022-07-23 21:27:54 +0200

    bugfix: new game images displayed again for new users

--- src/frontend/components/ImageTeaser.vue
+++ src/frontend/components/ImageTeaser.vue
@@ -9,44 +9,40 @@
     </div>
   </div>
 </template>
-<script lang="ts">
-import { defineComponent } from 'vue'
-import user from '../user'
+<script setup lang="ts">
+import { computed, onMounted, ref } from 'vue'
+import user, { User } from '../user'
 
-export default defineComponent({
-  props: {
-    image: {
-      type: Object,
-      required: true,
-    },
-  },
-  data: () => {
-    return {
-      me: user.getMe(),
-    }
-  },
-  computed: {
-    url(): string {
-      return this.image.url.replace('uploads/', 'uploads/r/') + '-375x0.webp'
-    },
-    canEdit(): boolean {
-      if (!this.me.id) {
-        return false
-      }
-      return this.me.id === this.image.uploaderUserId
-    },
-  },
-  emits: {
-    click: null,
-    editClick: null,
-  },
-  methods: {
-    onClick() {
-      this.$emit('click')
-    },
-    onEditClick() {
-      this.$emit('editClick')
-    },
-  },
+const props = defineProps({
+  image: { type: Object, required: true },
+})
+
+const emit = defineEmits<{
+  (event: 'click'): void
+  (event: 'editClick'): void
+}>()
+
+const me = ref<User|null>(null)
+
+const url = computed((): string => {
+  return props.image.url.replace('uploads/', 'uploads/r/') + '-375x0.webp'
+})
+
+const canEdit = computed((): boolean => {
+  if (!me.value || !me.value.id) {
+    return false
+  }
+  return me.value.id === props.image.uploaderUserId
+})
+
+const onClick = () => {
+  emit('click')
+}
+const onEditClick = () => {
+  emit('editClick')
+}
+
+onMounted(async () => {
+  me.value = user.getMe()
 })
 </script>
