0808e362e92410aa472039dcc4f68fb45871d6dc
Zutatensuppe <oo.para@gmail.com>
2022-12-26 16:35:56 +0100

    style fixes

--- src/frontend/views/Index.vue
+++ src/frontend/views/Index.vue
@@ -2,23 +2,44 @@
   <v-container :fluid="true" class="index-view">
     <h1>Running games</h1>
     <v-container :fluid="true" class="pl-0 pr-0 game-teasers-holder">
-      <GameTeaser v-for="(g, idx) in gamesRunning" :game="g" :key="idx" />
+      <GameTeaser
+        v-for="(g, idx) in gamesRunning"
+        :game="g"
+        :key="idx"
+        @goToGame="goToGame"
+        @goToReplay="goToReplay"
+      />
     </v-container>
 
     <h1>Finished games</h1>
     <v-container :fluid="true" class="pl-0 pr-0 game-teasers-holder">
-      <GameTeaser v-for="(g, idx) in gamesFinished" :game="g" :key="idx" />
+      <GameTeaser
+        v-for="(g, idx) in gamesFinished"
+        :game="g"
+        :key="idx"
+        @goToGame="goToGame"
+        @goToReplay="goToReplay"
+      />
     </v-container>
   </v-container>
 </template>
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
+import { useRouter } from 'vue-router';
 import { GameInfo } from '../../common/Types';
 import api from '../_api'
 import GameTeaser from './../components/GameTeaser.vue';
 
+const router = useRouter()
 const gamesRunning = ref<GameInfo[]>([])
 const gamesFinished = ref<GameInfo[]>([])
+
+const goToGame = ((game: any) => {
+  router.push({ name: 'game', params: { id: game.id } })
+})
+const goToReplay = ((game: any) => {
+  router.push({ name: 'replay', params: { id: game.id } })
+})
 
 onMounted(async () => {
   const res = await api.pub.indexData()
