eb3c5f60d5b81187af5bd4f1f3a6c5393e2fab2d
Zutatensuppe <oo.para@gmail.com>
2023-02-21 01:09:13 +0100

    symmetrical en/decode

--- src/server/Crypto.test.ts
+++ src/server/Crypto.test.ts
@@ -2,27 +2,15 @@ import { decrypt, encrypt } from './Crypto'
 import { describe, expect, it } from 'vitest'
 
 describe('Crypto.ts', () => {
-  describe('decrypt', () => {
+  describe('decrypt/decrypt', () => {
     const testCases = [
-      { encrypted: '82e8b148c660a5801819e26f26bd9c5b:15291cc243b27dc5f40227458ad69bb8', str: 'hello' },
-      { encrypted: '6e8cda1fcc2ed4c59e59d9aec695fa55:ebb8a61e24c498c108ccf83f9a805c78', str: 'hello' },
+      { encrypted: 'ZDRhOTcwZWY1NTQ2ZDEyMjE5ZjJiNjYwMTQ5OTYzNDhoZWxsbw==', decrypted: 'hello' },
+      { encrypted: 'ZDRhOTcwZWY1NTQ2ZDEyMjE5ZjJiNjYwMTQ5OTYzNDhiZXphdWJlcm5kc2lubmxpY2hlcnNjaHdhbkBtdWVsbC5pbw==', decrypted: 'bezauberndsinnlicherschwan@muell.io' },
     ]
 
-    testCases.forEach(({ encrypted, str }) => it('works', () => {
-      expect(decrypt(encrypted)).toEqual(str)
-    }))
-  })
-
-  it('encrypt', () => {
-    // encrypt creates random keys each time, so we can only test if
-    // encrypting and decrypting returns original value
-    const testCases = [
-      { str: 'hello' },
-      { str: 'haarrr@blub.com' },
-    ]
-
-    testCases.forEach(({ str }) => it('works', () => {
-      expect(decrypt(encrypt(str))).toEqual(str)
+    testCases.forEach(({ encrypted, decrypted }) => it('works', () => {
+      expect(decrypt(encrypted)).toEqual(decrypted)
+      expect(encrypt(decrypted)).toEqual(encrypted)
     }))
   })
 })
--- src/server/Crypto.ts
+++ src/server/Crypto.ts
@@ -1,28 +1,15 @@
 import config from './Config'
 import crypto from 'crypto'
 
-const algorithm = 'aes256'
-const inputEncoding = 'utf8';
-const outputEncoding = 'hex';
-const ivlength = 16  // AES blocksize
 const key = crypto.createHash('md5').update(config.secret).digest("hex")
-const iv = crypto.randomBytes(ivlength)
 
 export const encrypt = (str: string): string => {
-  const cipher = crypto.createCipheriv(algorithm, key, iv)
-  let ciphered = cipher.update(str, inputEncoding, outputEncoding)
-  ciphered += cipher.final(outputEncoding)
-  return iv.toString(outputEncoding) + ':' + ciphered
+  return Buffer.from(key + str, 'utf8').toString('base64')
 }
 
 export const decrypt = (str: string): string => {
-  const components = str.split(':')
-  const firstPart = components.shift() as string
-  const iv_from_ciphertext = Buffer.from(firstPart, outputEncoding)
-  const decipher = crypto.createDecipheriv(algorithm, key, iv_from_ciphertext)
-  let deciphered = decipher.update(components.join(':'), outputEncoding, inputEncoding)
-  deciphered += decipher.final(inputEncoding)
-  return deciphered
+  const decrypted = Buffer.from(str, 'base64').toString('utf8')
+  return decrypted.substring(key.length)
 }
 
 export default {
