806ed9ca972a88d6d85a1a727619eea193450e68
Zutatensuppe <oo.para@gmail.com>
2024-06-21 20:47:51 +0200

    fox twitch livestream url, move 'live on twitch' section up, fix sql files for new setup

--- db/dbpatches/12_leaderboard.sql
+++ db/dbpatches/12_leaderboard.sql
@@ -13,7 +13,3 @@ CREATE TABLE leaderboard (
 
 ALTER TABLE games
   ADD COLUMN pieces_count INTEGER NOT NULL DEFAULT 0;
-
-CREATE UNIQUE INDEX user_x_game_user_id_game_id ON user_x_game (user_id, game_id);
-
-CREATE UNIQUE INDEX leaderboard_name ON leaderboard (name);
