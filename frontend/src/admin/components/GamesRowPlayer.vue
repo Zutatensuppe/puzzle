e01f89d13038be4dc78d7ca1613f6fe5ede9258a
Zutatensuppe <oo.para@gmail.com>
2024-12-02 21:32:17 +0100

    split admin components

---
+++ frontend/src/admin/components/GamesRowPlayer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div :style="style">
+    {{ name }} ({{ points }})
+  </div>
+</template>
+<script setup lang="ts">
+import { computed, StyleValue } from 'vue'
+import { EncodedPlayer, EncodedPlayerIdx } from '../../../../common/src/Types'
+
+const props = defineProps<{
+  player: EncodedPlayer
+}>()
+
+const name = computed(() => props.player[EncodedPlayerIdx.NAME])
+const points = computed(() => props.player[EncodedPlayerIdx.POINTS])
+const style = computed((): StyleValue => {
+  if (props.player[5] === 'ukraine') {
+    return {
+      backgroundImage: 'linear-gradient(180deg, rgba(0,87,183,1) 0%, rgba(0,87,183,1) 50%, rgba(255,221,0,1) 50%)',
+      '-webkit-background-clip': 'text',
+      '-webkit-text-fill-color': 'transparent'
+    }
+  }
+  return {
+    color: props.player[EncodedPlayerIdx.COLOR] || 'white',
+  }
+})
+</script>
