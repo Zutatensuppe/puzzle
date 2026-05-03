b2c464b590ad9b697db3b04a6dfb823cfe83db11
Zutatensuppe <oo.para@gmail.com>
2026-05-03 10:46:56 +0200

    gzip game logs

--- server/src/lib/FileSystem.ts
+++ server/src/lib/FileSystem.ts
@@ -17,6 +17,7 @@ const exists = async (file: string): Promise<boolean> => await fs.access(file).t
 const readFile = async (file: string): Promise<string> => await fs.readFile(file, 'utf-8')
 const readFileRaw = async (file: string): Promise<Buffer> => await fs.readFile(file)
 const writeFile = async (file: string, text: string) => await fs.writeFile(file, text)
+const writeFileRaw = async (file: string, data: Buffer) => await fs.writeFile(file, data)
 const appendFile = async (file: string, text: string) => await fs.appendFile(file, text)
 const mkdir = async (dir: string) => await fs.mkdir(dir, { recursive: true })
 const readdir = async (dir: string) => await fs.readdir(dir)
@@ -39,6 +40,7 @@ export default {
   readFile,
   readFileRaw,
   writeFile,
+  writeFileRaw,
   appendFile,
   mkdir,
   readdir,
